59a55b31431325f4274270cee7398c949c76bbcb
aman9430kr <166622703+aman9430kr@users.noreply.github.com>
2024-04-10 19:08:57 +0530

    Add files via upload

---
+++ assignmentNo1.cpp
@@ -0,0 +1,54 @@
+
+        //Q1. How can you output “Physics” and “Wallah” in two different lines in C++?
+       // #include<iostream>
+      // using namespace std;
+      // int main(){
+        //cout<<"physics"<<endl<<"wallah";
+        //output :-
+        //physics
+        //wallah
+        //}
+
+    //Q2. Write a program to print 10 using 2 positive numbers less than 6 in C++ as output.
+//#include<iostream>
+// using namespace std;
+// int main(){
+    // int x=2;
+    // int y=5;
+    // int a=x*y;
+    // cout<<a;
+   // output :- 10
+//}
+
+
+   // Q3. How much space does the following data types take?
+   //.int = 4byte
+   // bool= 1 byte
+   // float = 4 byte
+
+  // Q4. What is the output of this program?
+    //     int a = 4;
+    // int b = 5;
+    // a++, b--;
+    //     cout << ++a << " " << b--;
+    //output -> 6 4
+
+//Q5. Write a program to find the circumference of a circle with radius 10 in C++.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float r;
+//     cout<<"enter radius"<<endl;
+//     cin>>r;
+//     float circumference=2*3.14*r;
+//     cout<<"circumference of circle is : "<<circumference;
+// }
+
+//Q6. How many of these can be a variable name ?
+
+//01Pwskills -> not variable name because its start from number
+// _FLOAT -> yes
+// int -> no because it is datatype name
+// FLOAT -> yes
+//You will succeed -> no
+ 
